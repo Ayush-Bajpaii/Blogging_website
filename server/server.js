42281393bef70cb44a2c656ae079bd4e817174eb
Ayush-Bajpaii <bajpaiayush12345@gmail.com>
2025-03-26 09:38:26 +0530

    updated server

--- server/server.js
+++ server/server.js
@@ -8,8 +8,8 @@ import { nanoid } from "nanoid";
 const server = express();
 let PORT = 3000;
 
-let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
-let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
 
 server.use(express.json());
 
